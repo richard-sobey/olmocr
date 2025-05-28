21ce19a0c1fcf747c4c88039b1cf849b824034fe
Richard Sobey <51140052+richard-sobey@users.noreply.github.com>
2025-05-28 19:23:03 +1000

    Update pipeline_utility.py

--- pipeline_utility.py
+++ pipeline_utility.py
@@ -160,7 +160,7 @@ async def sglang_server_task(args, semaphore):
         "warning",
         # Multimodal optimizations
         # "--disable-radix-cache",  # Required for qwen2-vl models
-        # "--chunked-prefill-size", "-1",  # Disable chunked prefill for multimodal
+        "--chunked-prefill-size", "8096",  # Disable chunked prefill for multimodal
         # "--schedule-policy", "fcfs"
     ]
     cmd.extend(mem_fraction_arg)
